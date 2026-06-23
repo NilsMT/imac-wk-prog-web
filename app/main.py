01100dc4a83983121e4cf76299ff87f486275501
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-06-23 10:09:36 +0200

    ajout de bcrypt

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
+#############################
+#init and imports
+#############################
+
 import sys, os
 #to be able to do service.XXX
 sys.path.insert(0, os.path.dirname(__file__))
 
 #import
 from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
 import database
 
 #services
 import service.example
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
+#############################
 #routes
+#############################
+
+#pages
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -20,7 +30,9 @@ def index():
 def example():
     return render_template("example.html", author = service.example.getAuthors())
 
+#############################
 #special behaviors
+#############################
 @app.teardown_appcontext
 def on_close(exception):
     database.disconnect_db()
