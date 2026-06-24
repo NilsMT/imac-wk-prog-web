7781912433cde95dc1afeda91196e89f1c549dbe
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-06-24 11:01:47 +0200

    removed example

--- app/model/example.py
+++
@@ -1,5 +0,0 @@
-#import
-from database import query_db
-
-def getAuthors():
-    return query_db("SELECT * FROM EXEMPLE ex WHERE ex.ACTIVE = ?",[1])
--- app/service/example.py
+++
@@ -1,7 +0,0 @@
-#import
-import model.example
-
-def getAuthors():
-    rows = model.example.getAuthors()
-
-    return [r["name"] for r in rows] #liste des noms (un peu tricky j'avoue)
