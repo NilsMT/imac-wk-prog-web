20a7c6e4c577cfc61fa0c23b9fa3f6e9303a7ca7
romanemarteaubazouni <romane.marteaubazouni@gmail.com>
2026-06-24 09:33:23 +0200

    finished model for events

--- app/model/event.py
+++ app/model/event.py
@@ -1,33 +1,25 @@
 #import
 from database import query_db
 from datetime import datetime
+# Found on : https://pytutorial.com/python-datetime-to-string-guide/
+currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-currentTime = datetime.now()
-
-# Select
+# SELECT
 def getAllEvents():
     return query_db("SELECT * FROM EVENT")
 
 def getAllNextEvents():
-    return query_db("SELECT * FROM EVENT " \
-    "WHERE start_date > ? " \
-    "ORDER BY (start_date) ASC", [currentTime])
+    return query_db("SELECT * FROM EVENT WHERE start_date > ? ORDER BY (start_date) ASC", [currentTime])
 
 def getNextEvent(id_user):
-    return query_db("SELECT * FROM EVENT E " \
-    "JOIN PARTICIPATION P ON E.id_event = P.id_event " \
-    "WHERE P.id_user = ? AND start_date > ? " \
-    "ORDER BY (start_date) ASC", [id_user, currentTime], True)
+    return query_db("SELECT * FROM EVENT E JOIN PARTICIPATION P ON E.id_event = P.id_event WHERE P.id_user = ? AND start_date > ? ORDER BY (start_date) ASC", [id_user, currentTime], True)
 
 # Verify if user is creator
-def isCreator(id_user):
-    return f"{id_user} == EVENT.id_user"
+def getMyEvents(id_user):
+    return query_db("SELECT * FROM EVENT WHERE EVENT.id_user = ?", [id_user])
 
-def getMyEvents(user):
-    return query_db(f"SELECT * FROM EVENT WHERE {isCreator(user)}")
-
-# Sorting
-# JSON : {"sort_1" : none, ASS ou DESC, "sort_2" : none, ASC ou DESC,...}
+# SORING
+# JSON : {"sort_1" : None, ASS ou DESC, "sort_2" : None, ASC ou DESC,...}
 
 def getEventsSorted(name, start_date, end_date, location):
     query = "SELECT * FROM EVENT"
@@ -49,28 +41,58 @@ def getEventsSorted(name, start_date, end_date, location):
     return query_db(query)
 
 # Only if user is creator
-# Delete event with boolean
-def deleteEvent(user, id_row):
-    return query_db(f"DELETE FROM EVENT E WHERE id_event = ? AND {isCreator(user)}", [id_row])
+# DELETE event
+def deleteEvent(id_user, id_row):
+    return query_db("DELETE FROM EVENT WHERE id_event = ? AND id_user = ?", [id_row, id_user])
 
-# Insert to create an event
+# INSERT to create an event
 # JSON : {"name" = ..., "start_date" = ...,..., "requis" = [{"entity" = ..., "attribute" = ..., "value" = ...}, {"entity" = ..., "attribute" = ..., "value" = ...}...]}
 
 def insertEvent(name, start_date, end_date, location, image, description, requis, id_user):
+    # Same way as sorting
+    query_begin = "INSERT INTO EVENT (name, start_date, end_date, location, id_user"
+    query_end = ") VALUES (?,?,?,?,?"
     values = [name, start_date, end_date, location, id_user]
-    query = "INSERT INTO EVENT VALUES(?,?,?,?,?"
-    if (image is None and description is None and requis is None):
-        return query_db(query + ")", values)
-    else:
-        if(image is not None):
-            query += " ,?"
-            values.append(image)
-        if(description is not None):
-            query += " ,?"
-            values.append(description)
-        if(requis is not None):
-            query += " ,?"
-            values.append(requis)
-        return query_db(query + ")")
+
+    if(image is not None):
+        query_begin += ", image"
+        query_end += ',?'
+        values.append(image)
+
+    if(description is not None):
+        query_begin += ", description"
+        query_end += ',?'
+        values.append(description)
+
+    # We create the event
+    query_db(query_begin + query_end + ")", values, True)
+    # For INSERT, query_db returns nb of rows affected (!= row like in SELECT)
+    # For ENTITY, we need the id_event : we use SELECT to get the row
+    id_event = query_db("SELECT id_event FROM EVENT WHERE name = ? AND start_date = ? AND id_user = ?", [name, start_date, id_user], True)["id_event"]
     
-# Update an event
+    if (requis is not None):
+        for eav in requis:
+            entity_type, attribut, value = eav["entity"], eav["attribute"], eav["value"]
+            # We create the entity
+            query_db("INSERT INTO ENTITY (type, id_event) VALUES (?,?)", [entity_type, id_event], True)
+            # To create the value, we need the entity id that we get from the row
+            id_entity = query_db("SELECT id_entity FROM ENTITY WHERE type = ? AND id_event = ?", [entity_type, id_event], True)["id_entity"]
+            # We create the value
+            query_db("INSERT INTO VALUE(id_attribut, id_entity, value) VALUES (?,?,?)", [attribut, id_entity, value])
+    return id_event
+
+# UPDATE an event : everything is updated, even if no changes
+def updateEvent(name, start_date, end_date, location, image, description, requis, id_row):
+    query_db("UPDATE EVENT SET name = ?, start_date = ?, end_date = ?, location = ?, image = ?, description = ? WHERE id_event = ?", [name, start_date, end_date, location, image, description, id_row])
+    # Same as INSERT
+    id_event = id_row
+    if (requis is not None):
+        for eav in requis:
+            entity_type, attribut, value = eav["entity"], eav["attribute"], eav["value"]
+            # We create the entity
+            query_db("INSERT INTO ENTITY (type, id_event) VALUES (?,?)", [entity_type, id_event], True)
+            # To create the value, we need the entity id
+            id_entity = query_db("SELECT id_entity FROM ENTITY WHERE type = ? AND id_event = ?", [entity_type, id_event])
+            # We create the value
+            query_db("INSERT INTO VALUE(id_attribut, id_entity, value) VALUES (?,?,?)", [attribut, id_entity, value])
+    return id_event
