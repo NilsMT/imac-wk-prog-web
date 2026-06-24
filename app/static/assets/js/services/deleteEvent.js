fa0e43a81c990e4ddcd4756a3a9b98b2c5fbb23a
romanemarteaubazouni <romane.marteaubazouni@gmail.com>
2026-06-24 17:47:04 +0200

    added possibility of deleting + added buttons
    Co-authored-by: Kellian Bredeau <bredeaukellian@gmail.com>

---
+++ app/static/assets/js/services/deleteEvent.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteEventButtons = document.querySelectorAll(".deleteEventButton");
+
+  deleteEventButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      console.log(this.id);
+      const eventId = this.id.split("-")[1];
+      console.log(`Deleting event with ID: ${eventId}`);
+      const response = fetch(`/event/${eventId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        console.error("Failed to delete event");
+      }
+    });
+  });
+});
